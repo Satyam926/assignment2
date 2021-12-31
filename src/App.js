cf45c4d29bf02a1f239b5308d1b9f95d94ec1646
Satyam926 <satyamt926@gmail.com>
2021-12-31 21:37:21 +0530

    ass2

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Employee from './Component/Employee';
+import './Component/Style.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Employee/>
+    </div>
+  );
+}
+
+export default App;
